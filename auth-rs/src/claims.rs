b8884a2bb72ebfc18b3c6de3b0cb9cc96fdf1523
west-github <westcoder@outlook.com>
2024-12-12 17:41:11 +0100

    Claim api updated

--- auth-rs/src/claims.rs
+++ auth-rs/src/claims.rs
@@ -1,130 +1,151 @@
-use std::{borrow::Cow, collections::HashMap};
+pub use claim::Claim;
+pub use claim_map::ClaimMap;
 
-/// Represents a claim used for authentication/authorization purposes.
-///
-/// The `Claim` struct encapsulates a type-value pair and related optional fields.
-/// This can represent concepts such as tokens, user claims, or access permissions.
-///
-/// # Fields
-///
-/// - `_type`: The type of claim being represented (e.g., `role`, `user_id`, `email`).  
-/// - `value`: The actual value of the claim (e.g., `user_id`, token, access key, or identifier).
-/// - `issuer`:  The issuer of the claim, identifying the origin or authority of this claim.
-/// - `subject`: The subject of the claim, representing context-specific information.  
-/// - `properties`: Additional properties that might be associated with the claim.  
-///
-/// # Example
-///
-/// ```rust
-/// use std::borrow::Cow;
-/// use std::collections::HashMap;
-/// use std_plus::string;
-///
-/// let claim = Claim::from("email", string!("auth-rs@example.com"))
-/// ```
-#[derive(new, Clone, PartialEq, Eq)]
-#[cfg_attr(
-    feature = "serde",
-    derive(serde::Serialize, serde::Deserialize)
-)]
-pub struct Claim {
-    _type: Cow<'static, str>,
-    value: String,
-    issuer: Option<String>,
-    subject: Option<ClaimInner>,
+mod claim {
+    use super::ClaimMap;
+    #[cfg(feature = "serde")]
+    use serde::{Deserialize, Serialize};
+    use std::{borrow::Cow, collections::HashMap};
 
-    /// alias - properties
-    pub props: Option<HashMap<String, String>>,
-}
-
-#[derive(new, Clone, PartialEq, Eq)]
-#[cfg_attr(
-    feature = "serde",
-    derive(serde::Serialize, serde::Deserialize)
-)]
-pub struct ClaimInner {
-    label: Cow<'static, str>,
-    claim: Vec<Claim>,
-}
-
-#[derive(new, Clone)]
-#[cfg_attr(
-    feature = "serde",
-    derive(serde::Serialize, serde::Deserialize)
-)]
-pub struct Claims {
-    claims: Vec<ClaimInner>,
-}
-
-impl Claim {
-    /// Sets the `issuer` for the claim.
+    /// Represents a claim used for authentication/authorization purposes.
     ///
-    /// # Arguments
-    /// * `issuer` - A string slice representing the issuer.
+    /// The `Claim` struct encapsulates a type-value pair and related optional fields.
+    /// This can represent concepts such as tokens, user claims, or access permissions.
+    ///
+    /// # Fields
+    ///
+    /// - `_type`: The type of claim being represented (e.g., `role`, `user_id`, `email`).  
+    /// - `value`: The actual value of the claim (e.g., `user_id`, token, access key, or identifier).
+    /// - `issuer`:  The issuer of the claim, identifying the origin or authority of this claim.
+    /// - `subject`: The subject of the claim, representing context-specific information.  
+    /// - `properties`: Additional properties that might be associated with the claim.  
     ///
     /// # Example
+    ///
+    /// ```rust
+    /// use std::borrow::Cow;
+    /// use std::collections::HashMap;
+    /// use std_plus::string;
+    ///
+    /// let claim = Claim::new("email", string!("auth-rs@example.com"))
     /// ```
-    /// let claim = Claim::new(...).with_issuer("IssuerName");
-    /// ```
-    pub fn with_issuer(self, issuer: &str) -> Self {
-        Self {
-            issuer: Some(issuer.into()),
-            ..self
-        }
+    #[derive(Clone, PartialEq, Debug, Eq)]
+    #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+    pub struct Claim {
+        _type: Cow<'static, str>,
+        value: String,
+        issuer: Option<String>,
+        subject: Option<ClaimMap>,
+
+        /// alias - properties
+        /// Option used cause we don't wanna carry empty hashmap all around
+        props: Option<HashMap<String, String>>,
     }
 
-    /// Sets the `subject` for the claim.
-    ///
-    /// # Arguments
-    /// * `subject` - A `Claims` struct representing the subject.
-    ///
-    /// # Example
-    /// ```
-    /// let claim = Claim::new(...).with_subject(ClaimInner::new(...));
-    /// ```
-    pub fn with_subject(self, subject: ClaimInner) -> Self {
-        Self {
-            subject: Some(subject),
-            ..self
+    impl Claim {
+        pub fn from(_type: &'static str, value: String) -> Self {
+            Self {
+                _type: Cow::Borrowed(_type),
+                value,
+                issuer: None,
+                props: None,
+                subject: None,
+            }
         }
+
+        /// Sets the `issuer` for the claim.
+        ///
+        /// # Arguments
+        /// * `issuer` - A string slice representing the issuer.
+        ///
+        /// # Example
+        /// ```
+        /// let claim = Claim::new(...).with_issuer("IssuerName");
+        /// ```
+        pub fn with_issuer(self, issuer: &str) -> Self {
+            Self {
+                issuer: Some(issuer.into()),
+                ..self
+            }
+        }
+
+        /// Sets the `subject` for the claim.
+        ///
+        /// # Arguments
+        /// * `subject` - A `Claims` struct representing the subject.
+        ///
+        /// # Example
+        /// ```
+        /// let claim = Claim::new(...).with_subject(ClaimMap::new(...));
+        /// ```
+        pub fn with_subject(self, subject: ClaimMap) -> Self {
+            Self {
+                subject: Some(subject),
+                ..self
+            }
+        }
+
+        // TODO -> Work on the properties api
+        // /// alias add_properties
+        // pub fn add_props(&mut self, key: &str, value: String) -> &mut Self {
+        //     self.get_props()
+        //         .insert(key.into(), value);
+        //     self
+        // }
+
+        // pub fn update_props(&mut self, key: &str, value: String) ->  {
+        //     self.props.map(|prop| )
+        // }
+
+        // pub fn
+
+        // fn get_props(&mut self) -> &mut HashMap<String, String> {
+        //     self.props.get_or_insert(HashMap::new())
+        // }
+    }
+}
+mod claim_map {
+    use super::Claim;
+    #[cfg(feature = "serde")]
+    use serde::{Deserialize, Serialize};
+    use std::{borrow::Cow, collections::HashMap};
+
+    // Add extend and other useful iterator
+
+    #[derive(Clone, PartialEq, Debug, Eq)]
+    #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+    pub struct ClaimMap {
+        // So we don't carry empty map all around when not used
+        claim_map: Option<HashMap<String, Vec<Claim>>>,
     }
 
-    /// Sets additional `properties` for the claim.
-    ///
-    /// # Arguments
-    /// * `props` - A map of key-value string pairs representing custom properties.
-    ///
-    /// # Example
-    /// ```
-    /// let claim = Claim::new(...).with_props(HashMap::new());
-    /// ```
-    pub fn with_props(self, props: HashMap<String, String>) -> Self {
-        Self {
-            props: Some(props),
-            ..self
+    impl ClaimMap {
+        pub fn new() -> Self {
+            Self { claim_map: None }
+        }
+
+        pub fn add_claims(&mut self, label: &str, claims: Vec<Claim>) -> &mut Self {
+            self.populate_claims((label.into(), claims));
+            self
+        }
+
+        fn populate_claims(&mut self, (label, claims): (String, Vec<Claim>)) {
+            let map = self
+                .claim_map
+                .get_or_insert(HashMap::new());
+
+            map.insert(label, claims);
         }
     }
 }
-/// Implements conversion from a tuple into a `Claim`.
-///
-/// This allows for easy creation of a `Claim` using a `(&str, String)` tuple.
-///
-/// # Arguments
-/// * `_type`: A static string slice representing the claim type.
-/// * `value`: A dynamically created string representing the claim value.
-///
-/// # Example
-/// ```rust
-/// let claim = Claim::from("email", String::from("auth-rs@example.com"));
-/// ```
-impl From<(&'static str, String)> for Claim {
-    fn from((_type, value): (&'static str, String)) -> Self {
-        Self {
-            _type: Cow::Borrowed(_type),
-            value,
-            issuer: None,
-            props: None,
-            subject: None,
-        }
+
+#[cfg(test)]
+mod test {
+    #![allow(unused_imports)]
+    use super::{claim::Claim, ClaimMap};
+
+    #[test]
+    fn test_claim() -> anyhow::Result<()> {
+        Ok(())
     }
 }
