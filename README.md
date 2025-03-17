ebe6629a78c82f2201be155e26c1af4e46bdcd48
West Sheriff <West Sheriff>
2025-03-17 16:11:15 +0100

    Initialize docs for documentation

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # theStack
 
-## Code Structures
+## Getting Started
+
+theStack is a framework designed to simplify web and ui development in Rust ecosystem. To learn more about it:
+
+- **Documentation:** [theStack Docs](https://thestack.github.io "theStack Docs")
+- **Repository:** [theStack GitHub](https://github.com/codetheproject/thestack "theStack Repo")
+
+## Community
+
+theStack is currently a work in progress (WIP), and we’re in the process of building a community. Stay tuned for an official community link—we’d love to have you join us!
+
+## Contribution
+
+theStack is open to contributions, and we highly appreciate your support, whether through code, monetary contributions, or other forms of assistance. Before contributing, please read our **[Contribution Guidelines](CONTRIBUTION.md)** to understand the coding style and project structure. If you have any suggestions, feel free to contact the maintainer:
+
+- **Discord:** [Insert Discord Link]
+- **Email:** [Insert Email Address]
+
+## Security
+
+If you discover a security vulnerability, please report it through our official security disclosure process. Submit your security concerns via [Security](SECURITY.md).
+
+---
+
+Stay tuned for updates as we improve theStack! 🚀
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,79 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'thestack',
+  tagline: 'WIP please check back later!',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/docs',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'codetheproject', // Usually your GitHub org/user name.
+  projectName: 'thestack', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'main',
+  trailingSlash: false,
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'thestack',
+      logo: {
+        alt: 'thestack logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/codetheproject/thestack',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      copyright: `Copyright © ${new Date().getFullYear()}`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
